e557379131a2d70645b5dc723c70c1bd8e10e8ae
unnivm <unnivm@gmail.com>
2012-12-10 11:41:13 +0000

    Apache licence 2.0

--- autosearch.js
+++ autosearch.js
@@ -1,5 +1,5 @@
 /*
-Copyright [2012] [Unni Vemanchery Mana]
+Copyright 2012 Unni Vemanchery Mana
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
